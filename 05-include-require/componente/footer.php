f02c734554aa241ae4a9643e3a7db4a815f7aab5
Yulieguerragavi <yulieguerra8@gmail.com>
2024-05-20 15:54:55 -0500

    feat:continua

---
+++ 05-include-require/componente/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <p class="mb-0 text-center">
+               Todos los derechos reservados, Multimedia 2024 🥰
+            </p>
+        </div>
+    </div>
+</footer>
---
+++ 06-funciones-php/index.php
@@ -0,0 +1,37 @@
+<?php
+
+    echo "ejecutando php";
+
+    //crear una funcion que diga si una persona es mayor o menor de edad
+
+    function ValidarEdad($edad){
+        if($edad >= 18){
+            echo "mayor de edad";
+        }else if($edad < 18){
+            echo "menor de edad";
+        }
+    }   
+
+    //llamando la funcion
+    ValidarEdad(isset($_POST['edad']) ? $_POST['edad'] :12);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Funciones php</h1>
+    <form action=<?php echo $_SERVER["PHP_SELF"] ?> method="POST"></form>
+    <label for="edad">Edad</label>
+    <input type="number" name="edad" id="edad">
+
+</body>
+</html>
+
