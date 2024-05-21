3b489a364a727dc5785e3d75491609b94a5330d8
Yulieguerragavi <yulieguerra8@gmail.com>
2024-05-21 16:09:59 -0500

    feat:continua

---
+++ 07-SQL/index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+//DOCUMENTACION: SQL w3school
+
+    //1. SQL SQL select
+    //SELECT materia, FROM profesores 
+
+    //2. SQL select distinc 
+    //SELECT `Documento` FROM `estudiantes` 
+    //solo extrae datos sin repetirlos 
+
+    //3. SQL Where
+    //SELECT * FROM `estudiantes` WHERE ID=1;
+    //captura valores unicos de columnas 
+
+    //4. SQL Order By
+    //
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
